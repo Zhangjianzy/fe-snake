1090e1e707cd9f2169357a58f5568d96e02c6cda
chandan <zhangjianzy@qq.com>
2025-09-16 07:24:52 +0800

    feat: add some css style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
+import { useEffect } from 'react';
 import GameView from './views/GameView';
 import { I18nProvider } from './i18n/I18nContext';
 import LanguageSelector from './components/LanguageSelector';
+import './App.css';
 
 function App() {
   // 设置游戏板尺寸，15列和20行
   const BOARD_WIDTH = 15;
   const BOARD_HEIGHT = 20;
+
+  // 全局动画效果
+  useEffect(() => {
+    // 添加页面载入动画类
+    document.body.classList.add('page-loaded');
+
+    // 清理函数
+    return () => {
+      document.body.classList.remove('page-loaded');
+    };
+  }, []);
 
   return (
     <I18nProvider>
